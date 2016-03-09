392b0fdb1f947f49357b07c7a7f315298fc3f448
lightninglord <mr.lo.mhs@gmail.com>
2016-03-09 13:07:07 -0800

    api exercise files

---
+++ api_exercise/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+app.set("view engine", "ejs");
+app.get('/', function(req,res){
+  res.send("hello, world")
+})
+app.get('/first', function(req, res) {
+  var url = 'https://myapi.profstream.com/api/23afb1/persons'
+    request(url, function(error, response, body){
+      console.log(body)
+      res.render("first", {
+        users: JSON.parse(body)
+      });
+    })
+});
+
+app.listen(3000)
