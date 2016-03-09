d83f94f0dd1936a5f5de9899166c18778a6d3e7d
lightninglord <mr.lo.mhs@gmail.com>
2016-03-09 13:52:51 -0800

    add create a new user function

--- api_exercise/index.js
+++ api_exercise/index.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
 var request = require('request');
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
 app.set("view engine", "ejs");
 app.get('/', function(req,res){
   res.send("hello, world")
 })
+var url = 'https://myapi.profstream.com/api/23afb1/persons'
 var request_and_render = function(template_name, res){
-  var url = 'https://myapi.profstream.com/api/23afb1/persons'
   request(url, function(error, response, body){
     res.render(template_name, {
       users: JSON.parse(body)
@@ -16,11 +20,31 @@ var request_and_render = function(template_name, res){
 app.get('/all', function(req, res) {
   request_and_render('all', res)
 });
-app.get('/first', function(res, res){
+app.get('/first', function(req, res){
   request_and_render('first', res)
 })
-app.get('/last', function(res, res){
+app.get('/last', function(req, res){
   request_and_render('last', res)
 })
+
+app.get('/new', function(req, res){
+  res.render('new')
+})
+
+
+app.post("/users", function(req, res) {
+    request({
+        method: "POST",
+        uri: url,
+        formData: {
+            firstname: req.body.first_name,
+            lastname: req.body.last_name,
+            email: req.body.email,
+            username: req.body.username
+        }
+    }, function(error, response, body) {
+        res.redirect("/all");
+    });
+});
 
 app.listen(3000)
